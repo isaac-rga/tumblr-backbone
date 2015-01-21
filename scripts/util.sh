1ead748f4885cccd2141d1ee04a4c0eceb1abd9b
Jesus Gollonet <jesusgollonet@gmail.com>
2015-01-21 21:32:10 +0100

    Add utility to start server in the background (using tmux)

--- scripts/util.sh
+++ scripts/util.sh
@@ -2,6 +2,18 @@
 
 # dependencies : github-markdown-preview
 
+tmux_session="tumblr-backbone"
+
+activate(){
+    create_tmux_session_if_not_exists $tmux_session
+    tmux send-keys -t $tumx_session:0 '. scripts/util.sh && start_server' Enter
+}
+
+start_server(){
+    (cd build
+    python -m SimpleHTTPServer)
+}
+
 preview_readme(){
     github-markdown-preview readme.md
 }
