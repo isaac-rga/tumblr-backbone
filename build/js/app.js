f7f0a26b1ff1e95fe2d0ff8084b842f4f05676de
Jesus Gollonet <jesusgollonet@gmail.com>
2015-01-21 23:19:09 +0100

    Wrap app in define. Add require.js config

--- build/js/app.js
+++ build/js/app.js
@@ -1,100 +1,110 @@
-(function(){
+define([
+    'jquery',
+    'underscore',
+    'backbone'
+], function(
+    $,
+    _,
+    Backbone
+){
 
-    'use strict';
-    // go to https://www.tumblr.com/oauth/apps and create an app in order to get an api key
-    var TUMBLR_API_KEY = 'YOU NEED TO REQUEST THIS'; 
-    var TumblrPost = Backbone.Model.extend();
+    console.log('hey!');
 
-    var TumblrPosts = Backbone.Collection.extend({
-        model: TumblrPost,
-        url: function(){
-            return 'http://api.tumblr.com/v2/blog/wowgreat.tumblr.com/posts?api_key=' + TUMBLR_API_KEY;
-        },
-        // jsonp
-        sync: function(method, model, options) {
-            return $.ajax(_.extend({
-                type: 'GET',
-                dataType: 'jsonp',
-                url: this.url(),
-                processData:true
-            }, options));
-        },
+        'use strict';
+        // go to https://www.tumblr.com/oauth/apps and create an app in order to get an api key
+        var TUMBLR_API_KEY = 'YOU NEED TO REQUEST THIS'; 
+        var TumblrPost = Backbone.Model.extend();
 
-        parse:function(r){
-            return r.response.posts;
-        }
-    });
+        var TumblrPosts = Backbone.Collection.extend({
+            model: TumblrPost,
+            url: function(){
+                return 'http://api.tumblr.com/v2/blog/wowgreat.tumblr.com/posts?api_key=' + TUMBLR_API_KEY;
+            },
+            // jsonp
+            sync: function(method, model, options) {
+                return $.ajax(_.extend({
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    url: this.url(),
+                    processData:true
+                }, options));
+            },
 
-    var TumblrBlog = Backbone.Model.extend({
-        POSTS_FETCHED: 'tumblrBlog:postsFetched',
-        initialize:function(){
-            _.bindAll(this, 'onPostsFetched');
-            this.posts = new TumblrPosts();
-        },
-
-        fetch:function(){
-            this.posts.fetch({
-                data:$.param({}),
-                success:this.onPostsFetched
-            });
-        },
-
-        onPostsFetched:function(){
-            this.trigger(this.POSTS_FETCHED);
-        },
-
-        getPosts:function(){
-            return this.posts.models;
-        }
-    });
-
-    var TumblrView= Backbone.View.extend({
-        el:'body',
-        initialize:function(options){
-            // this should go to the app model. views are dumb
-            this.model.on(this.model.POSTS_FETCHED, this.onPostsFetched,this);
-        },
-
-        onPostsFetched:function(){
-            this.createPostViews();
-            this.render();
-        },
-
-        createPostViews:function(){
-            this.postViews = _.map(this.model.getPosts(), this.createPostView ,this);
-        },
-
-        createPostView:function(p){
-            return new PostView({model:p});
-        },
-
-        appendPost:function(pv){
-            this.$el.append(pv.el);
-        },
-
-        render:function(){
-            _.each(this.postViews, this.appendPost,this);
-        }
-    });
-
-    var PostView = Backbone.View.extend({
-        initialize:function(){
-            var caption = $(this.model.get('caption')).text();
-            // we're only displaying photos
-            if (this.model.has('photos')){
-                var photo = this.model.get('photos')[0]; // grabbing only the first one to start with
-                var photoSizes = photo.alt_sizes;
-                photo = photo.alt_sizes[photoSizes.length-1].url;
-                var content = '<h2>' + caption + '</h2>';
-                content += '<img src="'+ photo+'" alt="" />';
-                this.el= $(content);
+            parse:function(r){
+                return r.response.posts;
             }
-        }
-    });
+        });
 
-    // this is our 'app model'
-    var t = new TumblrBlog();
-    var v = new TumblrView({model:t});
-    //t.fetch();
-    t.fetch();
-}());
+        var TumblrBlog = Backbone.Model.extend({
+            POSTS_FETCHED: 'tumblrBlog:postsFetched',
+            initialize:function(){
+                _.bindAll(this, 'onPostsFetched');
+                this.posts = new TumblrPosts();
+            },
+
+            fetch:function(){
+                this.posts.fetch({
+                    data:$.param({}),
+                    success:this.onPostsFetched
+                });
+            },
+
+            onPostsFetched:function(){
+                this.trigger(this.POSTS_FETCHED);
+            },
+
+            getPosts:function(){
+                return this.posts.models;
+            }
+        });
+
+        var TumblrView= Backbone.View.extend({
+            el:'body',
+            initialize:function(options){
+                // this should go to the app model. views are dumb
+                this.model.on(this.model.POSTS_FETCHED, this.onPostsFetched,this);
+            },
+
+            onPostsFetched:function(){
+                this.createPostViews();
+                this.render();
+            },
+
+            createPostViews:function(){
+                this.postViews = _.map(this.model.getPosts(), this.createPostView ,this);
+            },
+
+            createPostView:function(p){
+                return new PostView({model:p});
+            },
+
+            appendPost:function(pv){
+                this.$el.append(pv.el);
+            },
+
+            render:function(){
+                _.each(this.postViews, this.appendPost,this);
+            }
+        });
+
+        var PostView = Backbone.View.extend({
+            initialize:function(){
+                var caption = $(this.model.get('caption')).text();
+                // we're only displaying photos
+                if (this.model.has('photos')){
+                    var photo = this.model.get('photos')[0]; // grabbing only the first one to start with
+                    var photoSizes = photo.alt_sizes;
+                    photo = photo.alt_sizes[photoSizes.length-1].url;
+                    var content = '<h2>' + caption + '</h2>';
+                    content += '<img src="'+ photo+'" alt="" />';
+                    this.el= $(content);
+                }
+            }
+        });
+
+        // this is our 'app model'
+        var t = new TumblrBlog();
+        var v = new TumblrView({model:t});
+        //t.fetch();
+        t.fetch();
+});
--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,18 @@
+requirejs.config({
+
+    paths: {
+        'backbone':'lib/backbone', 
+        'underscore':'lib/underscore',
+        'jquery':'lib/jquery'
+    },
+
+    shim:{
+        'jquery': {exports: 'jQuery'},
+        'underscore': { exports: '_' },
+        'backbone': { deps: ['underscore', 'jquery'], exports: 'Backbone' },
+    }
+});
+
 require(['app'], function(){
     console.log('app!');
 });
