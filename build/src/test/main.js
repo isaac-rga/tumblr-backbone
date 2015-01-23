8b1c19c271a925852a70bcd93171e714935c3d9e
Jesus Gollonet <jesusgollonet@gmail.com>
2015-01-23 01:14:20 +0100

    Got a sane folder structure for tests
    http://bocoup.com/weblog/effective-unit-testing-with-amd/

--- build/src/test/main.js
+++ build/src/test/main.js
@@ -1,6 +1,18 @@
-require([], function(){
+// config ideas from 
+// http://bocoup.com/weblog/effective-unit-testing-with-amd/
+requirejs.config({
+    // we are using the same baseUrl as the app...
+    baseUrl:'../js',
+    // these need to be relative to the app
+    paths:{
+        'test': '../test/',
+        'spec' : '../test/spec/'
+    } 
+});
+
+require(['require-config'], function(){
     mocha.setup('bdd');
-    require(['model/post'],function(){
+    require(['spec/model/post'],function(){
         mocha.run(); 
     });
 });
--- build/src/test/spec/model/post.js
+++ build/src/test/spec/model/post.js
@@ -7,11 +7,7 @@ define([
     'use strict';
 
     var expect = chai.expect;
-        describe('sum', function(){
-            it('should be correct', function(){
-                expect(1 + 1).to.equal(2);
-            });
-        });
+
     describe('post model', function(){
         it('should exist', function(){
             expect(new PostModel()).to.exist();
