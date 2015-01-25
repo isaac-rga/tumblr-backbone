aa23ac2b775ee72b238dbfc20b9618c8a8e435aa
Jesus Gollonet <jesusgollonet@gmail.com>
2015-01-25 01:19:07 +0100

    Test blog.getPosts
    Stubbing out jquery.ajax to return 2 posts

--- build/src/test/spec/model/blog.js
+++ build/src/test/spec/model/blog.js
@@ -10,6 +10,27 @@ define([
         it('should exist', function(){
             expect(new BlogModel()).to.exist();
         });
+
+        describe('getPosts', function(){
+            beforeEach(function(){
+                // stub out jquery ajax to return 2 posts
+                sinon.stub($, "ajax").yieldsTo('success', {
+                    response:{
+                        posts:[{id:1}, {id:2}]
+                    }
+                });
+            });
+
+            it('returns the correct number of posts', function(){
+                var bm = new BlogModel();
+                bm.fetch();
+                expect(bm.getPosts().length).to.equal(2);
+            });
+
+            afterEach(function(){
+                $.ajax.restore();
+            });
+        });
     });
 
 });
