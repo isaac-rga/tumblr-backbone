9b977ce880be86287459809ef3a2d83e0ea751cc
Jesus Gollonet <jesusgollonet@gmail.com>
2015-01-25 01:27:32 +0100

    Test event triggering on blog model.
    Also rearrange test

--- build/src/test/spec/model/blog.js
+++ build/src/test/spec/model/blog.js
@@ -7,29 +7,32 @@ define([
     'use strict';
 
     describe('blog model', function(){
-        it('should exist', function(){
-            expect(new BlogModel()).to.exist();
+
+        beforeEach(function(){
+            // stub out jquery ajax to return 2 posts
+            sinon.stub($, "ajax").yieldsTo('success', {
+                response:{
+                    posts:[{id:1}, {id:2}]
+                }
+            });
         });
 
-        describe('getPosts', function(){
-            beforeEach(function(){
-                // stub out jquery ajax to return 2 posts
-                sinon.stub($, "ajax").yieldsTo('success', {
-                    response:{
-                        posts:[{id:1}, {id:2}]
-                    }
-                });
-            });
+        afterEach(function(){
+            $.ajax.restore();
+        });
 
-            it('returns the correct number of posts', function(){
-                var bm = new BlogModel();
-                bm.fetch();
-                expect(bm.getPosts().length).to.equal(2);
-            });
+        it('triggers POSTS_FETCHED when posts have been retrieved', function(){
+            var onPostsFetchedSpy = sinon.spy();
+            var bm = new BlogModel(); 
+            bm.on(bm.POSTS_FETCHED, onPostsFetchedSpy);
+            bm.fetch();
+            expect(onPostsFetchedSpy.calledOnce).to.be.true();
+        });
 
-            afterEach(function(){
-                $.ajax.restore();
-            });
+        it('returns the correct number of posts', function(){
+            var bm = new BlogModel();
+            bm.fetch();
+            expect(bm.getPosts().length).to.equal(2);
         });
     });
 
